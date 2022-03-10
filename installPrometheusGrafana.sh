cf5d453959614e955ce484a9a9eac20c7ba8f7eb
AnimeGir <animepowergirl1488@gmail.com>
2022-03-10 21:50:47 +0300

    sh script installrometheusgrafana

--- installPrometheusGrafana.sh
+++ installPrometheusGrafana.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-useradd --no-create-home --shell /bin/false prometheus  &&
-useradd --no-create-home --shell /bin/false node_exporter &&
+#install prometheus
+#check version - https://prometheus.io/download/
+#your_ip:9090
+
+VERSION="2.34.0"
+VERSION_NODE="1.3.1"
+
+export VERSION=$VERSION &&
+groupadd --system prometheus &&
+useradd --system -g prometheus -s /bin/false prometheus &&
+apt install -y wget tar &&
+wget "https://github.com/prometheus/prometheus/releases/download/v$VERSION-rc.1/prometheus-$VERSION-rc.1.linux-amd64.tar.gz" -O - | tar -xzv -C /tmp &&
 mkdir /etc/prometheus &&
 mkdir /var/lib/prometheus &&
-chown prometheus:prometheus /etc/prometheus &&
-chown prometheus:prometheus /var/lib/prometheus &&
-cd /opt/ &&
-wget https://github.com/prometheus/prometheus/releases/download/v2.26.0/prometheus-2.26.0.linux-amd64.tar.gz &&
-tar -xvf prometheus-2.26.0.linux-amd64.tar.gz &&
-cp /opt/prometheus-2.26.0.linux-amd64/prometheus /usr/local/bin/ &&
-cp /opt/prometheus-2.26.0.linux-amd64/promtool /usr/local/bin/ &&
-chown prometheus:prometheus /usr/local/bin/prometheus &&
-chown prometheus:prometheus /usr/local/bin/promtool &&
-cp -r /opt/prometheus-2.26.0.linux-amd64/consoles /etc/prometheus &&
-cp -r /opt/prometheus-2.26.0.linux-amd64/console_libraries /etc/prometheus &&
-cp -r /opt/prometheus-2.26.0.linux-amd64/prometheus.yml /etc/prometheus &&
-chown -R prometheus:prometheus /etc/prometheus/consoles &&
-chown -R prometheus:prometheus /etc/prometheus/console_libraries &&
-chown -R prometheus:prometheus /etc/prometheus/prometheus.yml &&
-prometheus /usr/local/bin/prometheus \
-        --config.file /etc/prometheus/prometheus.yml \
-        --storage.tsdb.path /var/lib/prometheus/ \
-        --web.console.templates=/etc/prometheus/consoles \
-        --web.console.libraries=/etc/prometheus/console_libraries &&
-"[Unit]
+cp -r /tmp/prometheus-$VERSION-rc.1.linux-amd64 /etc/prometheus &&
+rm -rf /tmp/prometheus-$VERSION-rc.1.linux-amd64 &&
+echo "global:
+  scrape_interval:     10s
+scrape_configs:
+  - job_name: 'prometheus'
+    static_configs:
+    - targets: ['localhost:9090']" > /etc/prometheus/prometheus.yml &&
+chown -R prometheus:prometheus /var/lib/prometheus /etc/prometheus &&
+chown prometheus:prometheus /usr/local/bin &&
+echo "[Unit]
 Description=Prometheus
 Wants=network-online.target
 After=network-online.target
@@ -32,26 +32,71 @@ After=network-online.target
 [Service]
 User=prometheus
 Group=prometheus
-Type=simple
 ExecStart=/usr/local/bin/prometheus \
     --config.file /etc/prometheus/prometheus.yml \
-    --storage.tsdb.path /var/lib/prometheus/ \
+    --storage.tsdb.path /var/lib/prometheus \
     --web.console.templates=/etc/prometheus/consoles \
     --web.console.libraries=/etc/prometheus/console_libraries
+ExecReload=/bin/kill -HUP $MAINPID
+[Install]
+WantedBy=default.target" > /etc/systemd/system/prometheus.service &&
+systemctl daemon-reload &&
+systemctl start prometheus.service &&
+systemctl enable prometheus.service &&
+systemctl status prometheus.service &&
+
+#install Node_exporter
+#check version - https://prometheus.io/download/
+#your_ip:9100
+
+export VERSION=$VERSION_NODE &&
+wget "https://github.com/prometheus/node_exporter/releases/download/v$VERSION_NODE/node_exporter-$VERSION_NODE.linux-amd64.tar.gz" -O - | tar -xzv -C /tmp &&
+cp  /tmp/node_exporter-$VERSION_NODE.linux-amd64/node_exporter /usr/local/bin &&
+chown -R prometheus:prometheus /usr/local/bin/node_exporter &&
+echo "[Unit]
+Description=Prometheus Node Exporter
+After=network.target
+
+[Service]
+Type=simple
+Restart=always
+User=prometheus
+Group=prometheus
+ExecStart=/usr/local/bin/node_exporter
 
 [Install]
-WantedBy=multi-user.target" > /etc/systemd/system/prometheus.service &&
+WantedBy=multi-user.target" > /etc/systemd/system/node_exporter.service &&
 systemctl daemon-reload &&
-systemctl start prometheus &&
-systemctl enable prometheus &&
-ufw allow 9090/tcp &&
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add – &&
-echo "deb https://packages.grafana.com/oss/deb stable main" | tee -a /etc/apt/sources.list.d/grafana.list &&
-apt-get update &&
-apt-get install grafana &&
-systemctl start grafana-server &&
-systemctl status grafana-server &&
+systemctl start node_exporter.service &&
+systemctl enable node_exporter.service &&
+systemctl status node_exporter.service &&
+echo "scrape_configs:
+- job_name: 'node'
+scrape_interval: 10s
+static_configs:
+      - targets: ['localhost:9100'] " >> /etc/prometheus/prometheus.yml &&
+systemctl reload prometheus.service &&
+
+#install grafana
+#your_ip:3000
+#login/password - admin/admin
+
+apt-get install -y software-properties-common wget apt-transport-https &&
+wget -q -O - https://packages.grafana.com/gpg.key | apt-key add - &&
+add-apt-repository "deb https://packages.grafana.com/oss/deb stable main" &&
+apt-get update && apt-get -y install grafana &&
+echo "apiVersion: 1
+datasources:
+- name: Prometheus
+type: prometheus
+access: proxy
+url: http://localhost:9090" > /etc/grafana/provisioning/datasources/prometheus.yml &&
+chown grafana:grafana /etc/grafana/provisioning/datasources/prometheus.yml &&
+systemctl start grafana-server.service &&
 systemctl enable grafana-server.service &&
-systemctl status prometheus &&
-prometheus --version &&
-promtool --version
+
+#check services status
+
+systemctl status prometheus.service &&
+systemctl status node_exporter.service &&
+systemctl status grafana-server.service
